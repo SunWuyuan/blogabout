ff3df6ffb54b7637c456c988ab6e45245f323425
SUNWUYUAN <88357633+SUNWUYUAN@users.noreply.github.com>
2022-07-31 10:59:36 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-row>
+      <v-btn color="primary" class="ma-3" href="https://status.wuyuan.dev/">网站状态</v-btn>
+      <v-col cols="auto">
+        <v-dialog transition="dialog-bottom-transition" max-width="600">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="primary" v-bind="attrs" v-on="on">作品</v-btn>
+          </template>
+          <template v-slot:default="dialog">
+            <v-card>
+              <v-toolbar color="primary" dark>我的作品</v-toolbar>
+
+<div style="height: 300px;overflow: auto;">
+              <v-btn color="primary" class="ma-2" href="https://baidu.com">工具1</v-btn>
+              <v-btn color="primary" class="ma-2" href="https://baidu.com">工具1</v-btn>
+
+</div>
+
+              <v-card-actions class="justify-end">
+                <v-btn text @click="dialog.value = false">关闭</v-btn>
+              </v-card-actions>
+            </v-card>
+          </template>
+        </v-dialog>
+      </v-col>
+      <v-btn color="primary" class="ma-3" href="https://wuyuan.dev/about/">关于我</v-btn>
+    </v-row>
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
