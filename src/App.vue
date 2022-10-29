05e8be03147fb4218a0ec9a3422f69c0b4503602
SUNWUYUAN <1847261658@qq.com>
2022-10-29 17:40:47 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,97 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <v-toolbar-title>我的网站</v-toolbar-title>
-
+    <!-- <v-app-bar app color="primary" dark>
+     <img src="https://cdn.wuyuan.dev/img/logoyuan.png" width="30"> <v-toolbar-title>孙悟元-知识创造一切</v-toolbar-title>
       <v-spacer></v-spacer>
 
-      <v-btn href="https://wuyuan.dev" target="_blank" text>
-        <span class="mr-2">作者博客</span>
+      <v-btn href="https://github.com/sunwuyuan" target="_blank" text>
+        <span class="mr-2">GitHub</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
-    </v-app-bar>
+    </v-app-bar>-->
+
 
     <v-main>
-
-
       <template>
+        <v-carousel cycle height="400" hide-delimiter-background show-arrows-on-hover v-ripple>
+          <v-carousel-item v-for="(slide, i) in slides" :key="i">
+            <v-sheet :color="colors[i]" height="100%">
+              <v-row class="fill-height" align="center" justify="center">
+                <div class="text-h2" style="color:#ffffff">
+                  {{ slide }}
+                </div>
+              </v-row>
+            </v-sheet>
+          </v-carousel-item>
+        </v-carousel>
+      </template>
+      <template>
+        <v-row justify="center">
 
 
+          <v-container>
+            <v-row v-bind="attrs" v-on="on">
+              <v-col v-for="card in cards" :key="card.title" :sm="card.flex">
+                <v-col>
+                  <v-card v-ripple min-height="150" @click="LinkTo(card.link)">
+                    <v-card-title class="text-h5">
+                      {{ card.title }}
+                    </v-card-title>
 
-        <v-container >
-          <v-row>
-            <v-col v-for="card in cards" :key="card.title" cols="12" sm="4">
-              <v-card>
-                <v-img :src="card.src" class="white--text align-end"
-                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.2)" height="auto">
-                  <v-card-title v-text="card.title"></v-card-title>
-                </v-img>
+                    <v-card-subtitle>{{ card.about }}</v-card-subtitle>
+                    <v-card-text>点击访问</v-card-text>
+                  </v-card>
+                </v-col>
 
-                <v-card-actions><v-text v-text="card.text" class="mx-2"></v-text>
-                  <v-spacer></v-spacer>
+              </v-col>
+            </v-row>
 
-                  <v-btn :href="card.link">访问
-                    <v-icon>mdi-open-in-new</v-icon>
-                  </v-btn>
+          </v-container>
 
- 
-                </v-card-actions>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
+        </v-row>
 
       </template>
+
+
     </v-main>
   </v-app>
 </template>
 
-<script>
-export default {
-}
-</script>
+
 
 <script>
-  export default {
-    data: () => ({
-      cards: [
-        { title: '博客', src: 'https://cdn.wuyuan.dev/img/wz_wuyuan~dev.jpeg',  link: 'https://wuyuan.dev' , text: '我的博客，也是个人主页哦'},
-        { title: '网站状态', src: 'https://cdn.wuyuan.dev/img/网页捕获_31-7-2022_143648_status.wuyuan.dev.jpeg',  link: 'https://status.wuyuan.dev/', text: '我的博客，也是个人主页哦'},
-        { title: '网页sans启动器', src: 'https://cdn.wuyuan.dev/img/20220731144055.png',  link: 'https://sans.wuyuan.dev', text: '我的博客，也是个人主页哦'},
-        { title: 'Best airlines', src: 'https://codeyuan.tk/bj.png',  link: '', text: '我的博客，也是个人主页哦'},
-      { title: 'Best airlines', src: 'https://codeyuan.tk/bj.png',  link: '', text: '我的博客，也是个人主页哦'},
-      
-      ],
-    }),
-  }
+
+export default {
+  data: () => ({
+    cards: [
+      { title: '博客', link: 'wuyuan.dev', about: '我的博客', flex: "3" },
+      { title: '网站状态', link: 'status.wuyuan.dev', about: '我的网站状态监控', flex: "3" },
+      { title: '网页sans启动器', link: 'sans.wuyuan.dev', about: '快速找到你想要的网页版sans游戏', flex: "3" },
+      { title: '元长', link: 'longlink.wuyuan.dev', about: '沙雕长链接', flex: "3" },
+      { title: '悟元短链接', link: 'url.wuyuan.dev', about: '一个自己在用的短链接服务', flex: "3" },
+      { title: '2103班', link: '2103.wuyuan.dev', about: '班级(非)官网', flex: "3" },
+      { title: 'cdm', link: 'cdm.wuyuan.dev', about: '快速下载网易云音乐资源', flex: "3" },
+      { title: '元启', link: 'yuanqi.wuyuan.dev', about: '浏览器起始页', flex: "3" },
+      { title: '资源站', link: 'drive.wuyuan.dev', about: '个人资源站', flex: "3" },
+    ],
+    colors: [
+      'primary',
+
+    ],
+    slides: [
+      '孙悟元 知识创造一切',
+
+    ],
+    dialog: false,
+    notifications: false,
+    sound: true,
+    widgets: false,
+
+    LinkTo(url) {
+      window.open("https://" + url, '_blank') // 新窗口打开外链接
+    },
+  }),
+
+
+}
 </script>
